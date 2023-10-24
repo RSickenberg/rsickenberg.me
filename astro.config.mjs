5c04061a8f6950617b9c49323c46599718b1537f
Romain Sickenberg <r.sickenberg@gmail.com>
2023-10-24 13:28:50 +0200

    deps(astro): Upgraded deps to astro V3 and removed deprecated content

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,10 @@ from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({ 
-    integrations: [tailwind(), image(), preact({compat: true})],
+    integrations: [tailwind(), preact({compat: true})],
     site: 'https://next.rsickenberg.me',
 });
