9350fad498f57aa2e9332233016c98e44c8beb3f
Sickenberg Romain <r.sickenberg@gmail.com>
2023-11-28 16:08:03 +0100

    feat(contact): added the contact methods along with some icons

---
+++ .astro/icon.d.ts
@@ -0,0 +1,9 @@
+declare module 'astro-icon' {
+    export type Icon = 
+		| "arcticons:mail"
+		| "arcticons:github"
+		| "arcticons:linkedin"
+		| "arcticons:x-twitter"
+		| "arcticons:document-viewer";
+
+  }
--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,14 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import preact from "@astrojs/preact";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({ 
-    integrations: [tailwind(), preact({compat: true})],
+    integrations: [tailwind(), preact({compat: true}), icon({
+        include: {
+            arcticons: ['mail', 'github', 'linkedin', 'x-twitter', 'document-viewer']
+        }
+    })],
     site: 'https://next.rsickenberg.me',
 });
