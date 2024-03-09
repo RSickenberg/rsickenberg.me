fc8edf8cf601e7e9490243aea3d6fb511b640ea0
Sickenberg Romain <r.sickenberg@gmail.com>
2024-03-09 18:32:37 +0100

    feat(sentry): added sentry for integration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,10 +5,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- Added Sentry integration
 
 ## [0.2.4] - 2024-03-09
 ### Added
-- Added Partytown for Gtag
+- Added PartyTown for Gtag
 
 ## [0.2.3] - 2024-03-09
 ### Security
@@ -123,7 +125,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 - Few issues on dark mode and event listener
 
-[unreleased]: https://github.com/RSickenberg/rsickenberg.me-next/compare/v0.2.4...HEAD
+[Unreleased]: https://github.com/RSickenberg/rsickenberg.me-next/compare/v0.2.4...HEAD
 [0.2.4]: https://github.com/RSickenberg/rsickenberg.me-next/compare/v0.2.3...v0.2.4
 [0.2.3]: https://github.com/RSickenberg/rsickenberg.me-next/compare/v0.2.1...v0.2.3
 [0.2.2]: https://github.com/RSickenberg/rsickenberg.me-next/compare/v0.2.1...v0.2.2
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,32 @@
-import {defineConfig} from 'astro/config';
-
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import preact from "@astrojs/preact";
 import icon from 'astro-icon';
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
+import sentry from "@sentry/astro";
+
+// https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), preact({
-        compat: true
-    }), icon({
-        include: {
-            arcticons: ['mail', 'github', 'linkedin', 'x-twitter', 'document-viewer']
-        }
-    }), partytown({
-        config: {
-            forward: ['dataLayer.push'],
-        }
-    })],
-    site: 'https://next.rsickenberg.me',
-    output: 'static',
+  integrations: [tailwind(), preact({
+    compat: true
+  }), icon({
+    include: {
+      arcticons: ['mail', 'github', 'linkedin', 'x-twitter', 'document-viewer']
+    }
+  }), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), sentry({
+    dsn: "https://b3cfcc72d00637196118bd93eeb135b4@o562906.ingest.us.sentry.io/4506882226323456",
+    sourceMapsUploadOptions: {
+      project: "rsickenberg-next",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    },
+  })],
+  site: 'https://next.rsickenberg.me',
+  output: 'static'
 });
