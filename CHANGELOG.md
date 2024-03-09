babaf4d0c7273c67c8fce528178116e8c2ab2b16
Sickenberg Romain <r.sickenberg@gmail.com>
2024-03-09 18:21:25 +0100

    feat(partytown): added gtag via partytown

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- Added Partytown for Gtag
 
 ## [0.2.3] - 2024-03-09
 ### Security
@@ -119,7 +121,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 - Few issues on dark mode and event listener
 
-[unreleased]: https://github.com/RSickenberg/rsickenberg.me-next/compare/v0.2.3...HEAD
+[Unreleased]: https://github.com/RSickenberg/rsickenberg.me-next/compare/v0.2.3...HEAD
 [0.2.3]: https://github.com/RSickenberg/rsickenberg.me-next/compare/v0.2.1...v0.2.3
 [0.2.2]: https://github.com/RSickenberg/rsickenberg.me-next/compare/v0.2.1...v0.2.2
 [0.2.1]: https://github.com/RSickenberg/rsickenberg.me-next/compare/v0.1.2...v0.2.1
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,24 @@
-import {
-    defineConfig
-}
-from 'astro/config';
+import {defineConfig} from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import preact from "@astrojs/preact";
 import icon from 'astro-icon';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({ 
-    integrations: [tailwind(), preact({compat: true}), icon({
+export default defineConfig({
+    integrations: [tailwind(), preact({
+        compat: true
+    }), icon({
         include: {
             arcticons: ['mail', 'github', 'linkedin', 'x-twitter', 'document-viewer']
-        },
+        }
+    }), partytown({
+        config: {
+            forward: ['dataLayer.push'],
+        }
     })],
     site: 'https://next.rsickenberg.me',
     output: 'static',
